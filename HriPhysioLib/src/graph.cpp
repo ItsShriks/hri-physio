5e3604adb6935943fec9f799f7ceb456b7e7e3af
Shrikar Nakhye <nakhyeshrikar@icloud.com>
2024-06-07 22:36:01 +0200

    refactor graph.cpp
    1. #include <stdexcept>
    2. bool Graph = void Graph
    3. assign instead of resize
    4. std::make_unique for initialising nbr.
    5. Added const to member functions where applicable.
    6. Used range-based loops in the constructor and Dijkstra method.
    7. std::out_of_range exception to handle invalid node indices in addEdge.
    8. auto for type deduction

--- HriPhysioLib/src/graph.cpp
+++ HriPhysioLib/src/graph.cpp
@@ -11,42 +11,32 @@
  */
 
 #include <HriPhysio/Core/graph.h>
-
-typedef std::vector< hriPhysio::Core::Edge >::iterator EdgeIter;
-typedef std::pair<int, int> pii;
+#include <stdexcept>
 
 using namespace hriPhysio::Core;
 
 
-Graph::Graph(int n) : num_nodes(n), num_edges(0) {
-
-    nbr.reset(new std::vector< Edge >[num_nodes]);
-    for (size_t idx = 0; idx < num_nodes; ++idx) {
-        nbr[idx].clear();
+Graph::Graph(int n) : num_nodes(n), num_edges(0), nbr(std::make_unique<std::vector<Edge>[]>(num_nodes)) {
+    for (auto& edges : nbr) {
+        edges.clear();
     }
 }
 
 
-Graph::~Graph() {
-    nbr.reset();
-}
+Graph::~Graph() = default;
 
-
-bool Graph::addEdge(int u, int v, double weight/*=1.0*/) {
-    
-    //-- Check to see if our u and v are inbounds?
-    if (u >= num_nodes || u < 0 || v >= num_nodes || v < 0) { return false; }
-
+void Graph::addEdge(int u, int v, double weight/*=1.0*/) {
+    if (u >= num_nodes || u < 0 || v >= num_nodes || v < 0) {
+        throw std::out_of_range("Node index out of range");
+    }
     //-- Add an ``undirected`` edge between the vertices.
     nbr[u].push_back(Edge(v, weight));
     nbr[v].push_back(Edge(u, weight));
     ++num_edges;
-
-    return true;
 }
 
 
-int Graph::getNumEdges() {
+int Graph::getNumEdges() const {
     return num_edges;
 }
 
@@ -64,28 +54,24 @@ void Graph::shortestPath(int source, int target, std::string& message) {
 
     //-- Recover the path from the previous 
     //-- nodes used to get to the target.
-    std::vector<int> path; path.clear();
-
-    int from = target;
-    path.push_back(from);
-    while(prev[from] != -1) {
-        from = prev[from];
+    std::vector<int> path;
+    for (int from = target; from != -1; from = prev[from]) {
         path.push_back(from);
     }
-
     //-- Reverse this so constructing the string is easier.
     std::reverse(path.begin(), path.end());
-    message = "";
-
     //-- If there was only one element in the path, 
     //-- then no such route exists.
     if (path.size() == 1) {
+        message.clear();
         return;
     }
-
+    message.clear();
     //-- Construct the string adding ``-`` between the nodes.
     for (size_t idx = 0; idx < path.size(); ++idx) {
-        if (idx) { message += "-"; }           //-- i.e. not zero.
+        if (idx !=0) {
+            message += "-";
+        }           //-- i.e. not zero.
         message += std::to_string(path[idx]);  //-- append the node to the message.
     }
 }
@@ -100,43 +86,35 @@ void Graph::dijkstra(int source) {
     std::priority_queue< pii, std::vector<pii>, std::greater<pii> > fringe;
 
     //-- Flush some buffers we will use.
-    dist.resize(num_nodes); 
-    prev.resize(num_nodes); 
-
-    std::fill(dist.begin(), dist.end(), -1);
-    std::fill(prev.begin(), prev.end(), -1);
-
-
+    dist.assign(num_nodes, -1);
+    prev.assign(num_nodes, -1);
     //-- Init the first ``step``.
     dist[source] = 0;
-    fringe.push( std::make_pair(dist[source], source) );
+    fringe.emplace(dist[source], source);
 
 
     //-- Take steps in the graph until a path is found!
     //-- If there is no path, we will exhaust all options.
     while (!fringe.empty()) {
-        
-        //-- Take the next best option.
-        pii next = fringe.top(); fringe.pop();
-
-        //-- What node are we using?
-        int from = next.second;
+        auto [currentDist, from] = fringe.top();
+        fringe.pop();
 
         //-- If we have already tried to use this vertex, don't bother.
-        if (used[from]) { continue; }
+        if (used[from]) {
+            continue;
+        }
 
         //-- Don't try to revisit here in the future.
         used[from] = true;
 
         //-- Check each neighbor from u to v.
-        for (EdgeIter iter = nbr[from].begin(); iter != nbr[from].end(); ++iter) {
-            
-            int to      = iter->to;
-            int weight  = iter->weight + next.first;
+        for (const auto& edge : nbr[from]) {
+            int to = edge.to;
+            int weight = edge.weight + currentDist;
 
-            //-- If this has already been used, don't bother processing.
-            if (used[to]) { continue; }
-
+            if (used[to]) {
+                continue;
+            }
             //-- If this node has not been visited yet, or if 
             //-- we have found a better path to get here, update.
             if (dist[to] == -1 || weight < dist[to]) {
@@ -144,7 +122,7 @@ void Graph::dijkstra(int source) {
                 dist[to] = weight;
                 prev[to] = from;
 
-                fringe.push( std::make_pair(dist[to], to) );
+                fringe.emplace( dist[to], to) );
             }
         }
     }
